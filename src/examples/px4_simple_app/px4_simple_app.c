e07cdaeca277881d997e36dea271517eb9dbb0a8
ethan2zhong <ehtan_zsc@aliyun.com>
2017-06-29 18:01:49 +0800

    git push test from Ethan!

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -56,7 +56,7 @@ __EXPORT int px4_simple_app_main(int argc, char *argv[]);
 int px4_simple_app_main(int argc, char *argv[])
 {
 	PX4_INFO("Hello Sky!");
-    PX4_INFO("Ethan's PX4 Test!!!");
+    PX4_INFO("Ethan's PX4 Test!");
 
 	/* subscribe to sensor_combined topic */
 	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
